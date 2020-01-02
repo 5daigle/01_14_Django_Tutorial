ad1c7d8aa5889fe3448c183ba0ccd6ee337259e6
Samuel Daigle <samuel.daigle.2@ulaval.ca>
2020-01-02 14:04:00 -0500

    Rendu chapitre 4 login

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from boards import views
+from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('signup/', accounts_views.signup, name='signup'),
+    path('login/', auth_views.LogoutView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('boards/<int:pk>/', views.board_topics, name='board_topics'),
+    path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
 ]
